2ba9f082f688394c47d14cb37d23239f42849f68
balasubramanian <companyofbala@gmail.com>
2020-05-23 19:50:02 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express=require("express");
+var app=express();
+
+app.get("/", function(req, res){
+	res.send("Hi There From Express!");
+});
+app.get("/hi", function(req, res){
+	res.send("Hi Da!");
+});
+app.get("/bye", function(req, res){
+	res.send("Bye Da!");
+});
+app.get("*", function(req,res){
+	res.send("Error Not Found!");
+});
+app.listen(process.env.PORT||3000, process.env.IP,function(){
+	console.log("Port Running!");
+});
